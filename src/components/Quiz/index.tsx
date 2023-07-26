253a6b5d2ee94c811d14d9494c9770e5780c1a4c
Seyyid Yiğit <seyyid364@gmail.com>
2023-07-26 17:07:16 +0300

    The quiz component is organized to receive questions according to the parameter and the highest score is kept in local storage.

--- src/components/Quiz/index.tsx
+++ src/components/Quiz/index.tsx
@@ -1,50 +1,80 @@
 import React, { useState, useEffect } from "react";
-import { Question } from "./question";
 import "./index.css";
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
+export interface QuizQuestion {
+  questionText: string;
+  answerOptions: AnswerOption[];
+}
+
+interface AnswerOption {
+  answerText: string;
+  isCorrect: boolean;
+}
 // A component to render a single question
 function QuestionComp({ question, index, total, onAnswer, style }) {
+
+  // Destructure the question prop
+  const { questionText, answerOptions } = question;
   return (
     <div className="question-section" style={style}>
+      {" "}
       <div className="question-count">
         <span>Question {index + 1}</span>/{total}
       </div>
-      <div className="question-text">{question.questionText}</div>
+      <div className="question-text">{questionText}</div>
       <div className="answer-section">
-        {question.answerOptions.map((answerOption) => (
+        {answerOptions.map((answerOption) => (
           <button
             className="quiz__button"
             onClick={() => onAnswer(answerOption.isCorrect)}
           >
-            {answerOption.answerText}
+            <Typography
+              variant="body1"
+              sx={{ color: "white", textAlign: "start" }}
+            >
+              {" "}
+              {answerOption.answerText}
+            </Typography>
           </button>
         ))}
       </div>
     </div>
   );
 }
-
-function Quiz(questions: Question[]) {
+const Quiz = (questions: QuizQuestion[]) => {
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   const [score, setScore] = useState<number>(0);
   const [showScore, setShowScore] = useState<boolean>(false);
   const [restart, setRestart] = useState<boolean>(false);
+  const [highScore, setHighScore] = useState<number>(0);
 
   questions = Object.values(questions);
 
   useEffect(() => {
-      setCurrentQuestion(0);
-      setScore(0);
-      setShowScore(false);
-
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+    setHighScore(
+      localStorage.getItem("score")
+        ? parseInt(localStorage.getItem("score"))
+        : 0
+    );
   }, [restart]);
 
-
   const handleAnswerOptionClick = (isCorrect: boolean) => {
-    console.log(isCorrect);
     if (isCorrect) {
       setScore(score + 1);
+      var storageScore = localStorage.getItem("score");
+      if (storageScore === null) {
+        localStorage.setItem("score", JSON.stringify(score + 1));
+        setHighScore(score + 1);
+      } else {
+        if (score >= parseInt(storageScore)) {
+          localStorage.setItem("score", JSON.stringify(score + 1));
+          setHighScore(score + 1);
+        }
+      }
     }
 
     const nextQuestion = currentQuestion + 1;
@@ -58,13 +88,19 @@ function Quiz(questions: Question[]) {
   return (
     <div className="quiz">
       <div className="app__quiz">
+        {/* Use ternary operators to simplify conditional rendering */}
         {showScore || questions.length === 0 ? (
           <div className="score-section">
             {questions.length === 0 ? (
               <div>There are no questions</div>
             ) : (
               <div className="quiz__container">
-                You scored {score} out of {questions.length}
+                <Typography variant="h6" gutterBottom>
+                  Your score is {score} out of {questions.length}
+                </Typography>
+                <Typography variant="h6" gutterBottom>
+                  Your highest score : {highScore}
+                </Typography>
                 <Button
                   variant="contained"
                   onClick={() => setRestart(!restart)}
@@ -85,8 +121,8 @@ function Quiz(questions: Question[]) {
               justifyContent: "center",
             }}
           >
-            {/* // Render a list of questions and only show the one that matches the currentQuestion index */}
-            {questions.map((question: Question, index) => (
+            {/* Use map to render a list of questions and only show the one that matches the currentQuestion index */}
+            {questions.map((question: QuizQuestion, index) => (
               <QuestionComp
                 key={index + question.questionText.length}
                 question={question}
@@ -103,6 +139,6 @@ function Quiz(questions: Question[]) {
       </div>
     </div>
   );
-}
+};
 
 export default Quiz;
