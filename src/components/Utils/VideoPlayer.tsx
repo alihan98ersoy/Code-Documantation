b20b49bb8826cafef40d5794728df1e86dbc3554
Seyyid Yiğit <seyyid364@gmail.com>
2023-08-01 14:22:34 +0300

    No needed other props in ReactPlayer after adding control prop.

--- src/components/Utils/VideoPlayer.tsx
+++ src/components/Utils/VideoPlayer.tsx
@@ -17,109 +17,20 @@ import { useTheme } from "@mui/material/styles"; // add this line
 import useMediaQuery from "@mui/material/useMediaQuery"; // add this line
 
 function VideoPlayer({ videoUrl }) {
-  const [volume, setVolume] = useState(0.5); // add this line
-  const [muted, setMuted] = useState(false); // add this line
-  const {
-    playerRef,
-    playing,
-    playbackRate,
-    currentTime,
-    duration,
-    handlePlayPause,
-    handleRewind,
-    handleFastForward,
-    handleProgress,
-  } = useVideoPlayer();
 
   const theme = useTheme(); // add this line
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm")); // add this line
 
   return (
     <Container maxWidth="md">
-      {" "}
-      {/* add this line */}
       <ReactPlayer
-        ref={playerRef}
         url={videoUrl}
-        playing={playing}
-        playbackRate={playbackRate}
-        volume={volume}
-        muted={muted}
-        fastForward={true}
-        onProgress={handleProgress}
         width="100%" // add this line
-        height="480px"
-        pip={true}
+        height={isSmallScreen ? "360px" : "480px"} // add this line
+        controls={true}
       />
-      <Stack direction="row" spacing={2} alignItems="center">
-        <Paper sx={{ display: "flex", alignItems: "center", width: "100%", justifyContent: "space-around" }}>
-          <IconButton onClick={handleRewind}>
-            <FastRewindIcon />
-          </IconButton>
-          <IconButton onClick={handlePlayPause}>
-            {playing ? <PauseIcon /> : <PlayArrowIcon />}
-          </IconButton>
-          <IconButton onClick={handleFastForward}>
-            <FastForwardIcon />
-          </IconButton>
-          <Slider
-            value={(currentTime / duration) * 100}
-            onChange={(e, newValue: number) =>
-              handleProgress({ played: newValue / 100 })
-            }
-            sx={{
-              width: isSmallScreen ? 100 : 200, // change this line
-              color: "primary.main",
-              "& .MuiSlider-thumb": {
-                borderRadius: "50%",
-                width: 12,
-                height: 12,
-              },
-              "& .MuiSlider-track": {
-                height: 4,
-              },
-            }}
-          />
-          <Typography variant="caption" color="text.secondary">
-            {currentTime.toFixed(2)}
-          </Typography>
-          <Typography variant="caption" color="text.secondary">
-            /
-          </Typography>
-          <Typography variant="caption" color="text.secondary">
-            {duration.toFixed(2)}
-          </Typography>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <IconButton onClick={() => setMuted(!muted)}>
-              <VolumeUpIcon
-                sx={
-                  muted
-                    ? { color: "primary.main" }
-                    : { color: "secondary.main" }
-                }
-              />
-            </IconButton>
-            <Slider
-              value={volume * 100}
-              onChange={(e, newValue: number) => setVolume(newValue / 100)}
-              sx={{
-                width: isSmallScreen ? 50 : 100, // change this line
-                color: "secondary.main",
-                "& .MuiSlider-thumb": {
-                  borderRadius: "50%",
-                  width: 10,
-                  height: 10,
-                },
-                "& .MuiSlider-track": {
-                  height: 3,
-                },
-              }}
-            />
-          </Box>
-        </Paper>
-      </Stack>
     </Container>
   );
 }
 
-export default React.memo(VideoPlayer);
+export default VideoPlayer;
