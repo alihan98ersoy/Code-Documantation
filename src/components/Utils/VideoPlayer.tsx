efa95c48e389a4480c2974edf0df650dbcf88f42
Seyyid Yiğit <seyyid364@gmail.com>
2023-08-01 14:42:40 +0300

    Fixed navbar and hero title and subtitle responsive problem.

--- src/components/Utils/VideoPlayer.tsx
+++ src/components/Utils/VideoPlayer.tsx
@@ -1,17 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactPlayer from "react-player/lazy";
-import { useVideoPlayer } from "./hooks/useVideoPlayer";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import Slider from "@mui/material/Slider";
-import IconButton from "@mui/material/IconButton";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import PauseIcon from "@mui/icons-material/Pause";
-import FastForwardIcon from "@mui/icons-material/FastForward";
-import FastRewindIcon from "@mui/icons-material/FastRewind";
-import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import Container from "@mui/material/Container"; // add this line
 import { useTheme } from "@mui/material/styles"; // add this line
 import useMediaQuery from "@mui/material/useMediaQuery"; // add this line
